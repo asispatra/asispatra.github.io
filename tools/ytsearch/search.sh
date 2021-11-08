43598badd715de4e9edc2a661b70f4d63b2c1be8
Asis Patra <asis.patra0425@gmail.com>
2021-11-08 19:33:56 +0530

    Delete search.sh

--- tools/ytsearch/search.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-TARGET=$(echo "$1" | sed 's/\ /%20/g')
-#curl "https://suggestqueries-clients6.youtube.com/complete/search?client=youtube&ds=yt&q=${TARGET}" | sed 's/.*(\(.*\)).*/\1/'
-#curl "https://suggestqueries-clients6.youtube.com/complete/search?client=youtube&ds=yt&q=${TARGET}" | sed 's/.*(\(.*\)).*/\1/' | jq "{data: .}"
-curl "https://suggestqueries-clients6.youtube.com/complete/search?client=youtube&ds=yt&q=${TARGET}" | sed 's/.*(\(.*\)).*/\1/' | jq ".[1][] | {data: .[0], value: .[2]}" | jq -s .
